5a1ba8093c9f72855bb20885a703f82570e9bc00
Maxime Sateur <maxime.sateur@gmail.com>
2018-07-08 20:19:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ function theBeatlesPlay(musicians, instruments) {
 
   return arr
 }
+
+function johnLennonFacts(facts){
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+let i= 0
+
+while ([i< facts.length]){
+  shoutedFacts.push(`${facts[i]}!!!`)
+}
+return shoutedFacts
+}
