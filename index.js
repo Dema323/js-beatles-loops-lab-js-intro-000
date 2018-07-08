1250d0b254477205a9a26bf8ab18775848855f18
Maxime Sateur <maxime.sateur@gmail.com>
2018-07-08 20:14:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  const array = []
+
+  for(let i=0, 1 = musicians.lenght; i<1;i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}))
+  }
+  return array
+}
